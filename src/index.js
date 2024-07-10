4bfac82dd767c5b39c6ad7371af9de8fd1ccfbc8
luckystar628 <luckystar000628@gmail.com>
2024-07-11 04:03:56 +0900

    update code

--- src/index.js
+++ src/index.js
@@ -6,6 +6,20 @@ const axios = require('axios');
 
 const bot = new TelegramBot(TOKEN, { polling: true });
 
+const http = require('http');
+
+
+const port = process.env.PORT || 5000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
 bot.on('message', async msg => {
   try {
     const chatId = msg.chat.id;
